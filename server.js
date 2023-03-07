a936166021552f16f6d988da4ca0aeb0a4694660
yuum1276 <dyalsla0105@gmail.com>
2023-03-07 12:04:08 +0900

    nodejs websocket 공부중

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ const WebSocket = require('ws');
 
 const port = 3003;
 const server = http.createServer(express);
-const wss = new WebSocket.Server({server})
+const wss = new WebSocket.Server({server}) // 웹소켓 서버 생성
 
-wss.on('connection', function connection(ws) {
-    ws.on('message', function incoming(data){
+wss.on('connection', function connection(ws) { // 웹소켓 서버 실행
+    ws.on('message', function incoming(data) { // 데이터 수신 
         wss.clients.forEach(function each(client){
             if(client !== ws && client.readyState === WebSocket.OPEN){
-                client.send(data);
+                client.send(data); // 클라이언트에게 답장
             }
         })
     })
